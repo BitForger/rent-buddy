9c8f939ffca7d373f33964bce0ef19526a53a445
Noah Kovacs <kovacsn66@gmail.com>
2022-01-09 03:42:38 -0500

    chore: update code for monorepo

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,18 +1,18 @@
-import { Global, Module } from "@nestjs/common";
-import { ServicesModule } from "./shared/services/services.module";
-import { UserController } from "./controllers/user/user.controller";
-import { MongooseModule, MongooseModuleOptions } from "@nestjs/mongoose";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { User, UserSchema } from "./db/user.schema";
-import { Charge, ChargeSchema } from "./db/charge.schema";
-import { ChargeController } from "./controllers/charge/charge.controller";
-import { LoggerModule, Params } from "nestjs-pino";
+import { Global, Module } from '@nestjs/common';
+import { ServicesModule } from './shared/services/services.module';
+import { UserController } from './controllers/user/user.controller';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User, UserSchema } from './db/user.schema';
+import { Charge, ChargeSchema } from './db/charge.schema';
+import { ChargeController } from './controllers/charge/charge.controller';
+import { LoggerModule, Params } from 'nestjs-pino';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: '../../.env',
       isGlobal: true,
     }),
     LoggerModule.forRootAsync({
